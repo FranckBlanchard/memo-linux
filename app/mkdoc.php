d433aecbb40041b4865014d32ae94b910bf1a2a5
Franck Blanchard <info@aztequemedia.com>
2015-11-21 21:54:50 -0500

    Mise place de la structure php de génération du fichier html

--- app/mkdoc.php
+++ app/mkdoc.php
@@ -1,4 +1,12 @@
 <?php
+echo __DIR__;
+/**
+ * On charge l'autoloarder de composer
+ */
+$fileAutoload='../vendor/autoload.php';
+if (file_exists($fileAutoload)) {
+    include '$fileAutoload';
+}
 
-include 'vendor/autoload.php';
-echo $argv[0];
+Twig_Autoloader::register();
+$twigLoader=new Twig_Loader_Filesystem('templates');
